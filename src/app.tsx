37edaad32d8314415d8ee415fd3faa99b9d6e08a
baixingyu <baixingyu@beisen.com>
2022-04-02 20:24:13 +0800

    fix:修复错误

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 const App: React.FC = ({}) => {
-  return <div className="hello-world">
+  return (
+  <div className="hello-world">
     hello world
-  </div>;
+  </div>)
 };
 const rootElement = document.querySelector("#app");
 const root = ReactDOM.createRoot(rootElement!);
