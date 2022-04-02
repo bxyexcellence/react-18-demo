2e29bba92c552b417852097b5852cc6734f96fb6
baixingyu <baixingyu@beisen.com>
2022-04-02 19:57:11 +0800

    init:初始化项目

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+const App: React.FC = ({}) => {
+  return <div className="hello-world">
+    hello world
+  </div>;
+};
+const rootElement = document.querySelector("#app");
+const root = ReactDOM.createRoot(rootElement!);
+root.render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const html = require("html-webpack-plugin");
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  output: {
+    path: path.resolve(process.cwd(), "./dist"),
+    filename: '[name].[hash].js'
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".tsx", ".jsx", ".json"],
+    alias: {
+      "&": path.resolve(process.cwd(), "./src"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|ttf|eot|wav|mp3|mp4|avi|svg|woff(2)?)(\?[^('|")]*)?$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(scss|css|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env'],
+              ["@babel/preset-typescript"],
+              ["@babel/preset-react"]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+   // hot:true
+    historyApiFallback: true,
+    proxy: {
+      //context: '/hello',
+     /*  target: 'localhost:3000',  // 代理跨域目标接口 将后端代理到前端
+      changeOrigin: true,
+      secure: false,  // 当代理某些https服务报错时用
+      cookieDomainRewrite: false ,// 可以为false，表示不修改 */
+     /*  '/api': {
+        changeOrigin: true,
+        secure: false,  // 当代理某些https服务报错时用
+        cookieDomainRewrite: false ,// 可以为false，表示不修改
+        target: 'http://localhost:3000/api',
+        pathRewrite: { '^/api': '' },
+      }, */
+    }
+  },
+  plugins: [
+    new html({template: './index.html'})
+  ]
+};
